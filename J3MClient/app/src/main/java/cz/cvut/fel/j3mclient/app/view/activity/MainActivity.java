a5c01365c49f626a079f95677aed9ab7398a7da1
Jan Herzan <jan.herzan@gmail.com>
2014-12-10 15:38:25 +0100

    ListView přidáno do klienta (zatím bez importu dat ze servru).

--- J3MClient/app/src/main/java/cz/cvut/fel/j3mclient/app/view/activity/MainActivity.java
+++ J3MClient/app/src/main/java/cz/cvut/fel/j3mclient/app/view/activity/MainActivity.java
@@ -4,7 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import cz.cvut.fel.j3mclient.app.R;
+
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
 @EActivity
@@ -14,8 +22,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        String[] data = {"TEST1", "TEST2", "TEST3", "TEST4", "TEST5", "TEST6"};
+        this.populateOrderNodes(data);
+        this.registerOrderNodesHandlers();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,4 +46,23 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void populateOrderNodes(String[] data) {
+        ListView orderDataList = (ListView) this.findViewById(R.id.ordersListView);
+        ArrayAdapter<String> textViewAdapter = new ArrayAdapter<String>(this, R.layout.order_node, data);
+        orderDataList.setAdapter(textViewAdapter);
+    }
+
+    private void registerOrderNodesHandlers() {
+        ListView orderDataList = (ListView) this.findViewById(R.id.ordersListView);
+
+        orderDataList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Position " + position + " was clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
 }
